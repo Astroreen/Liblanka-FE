54f653120295ba932a4f6841df80518dd1f201f6
astroreen <astroreen@gmail.com>
2025-09-02 05:23:59 +0300

    New port on localhost

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,16 +1,16 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 
 export const BASE_URL = isDevelopment
-  ? "http://localhost:8080/api/v1"
-  : "https://www.liblanks.lt/api/v1"; //url to api
+    ? "http://localhost:7890/api/v1"
+    : "https://www.liblanks.lt/api/v1"; //url to api
 
 export const ENDPOINTS = {
-  login: "/auth/login",
-  products: "/storage/products",
-  product_types: "/storage/products/types",
-  product_sizes: "/storage/products/sizes",
-  product_colors: "/storage/products/colors",
-  product_info: "/storage/products/information",
-  products_filter: "/storage/products/filter",
-  userDetails: "/user/details",
+    login: "/auth/login",
+    products: "/storage/products",
+    product_types: "/storage/products/types",
+    product_sizes: "/storage/products/sizes",
+    product_colors: "/storage/products/colors",
+    product_info: "/storage/products/information",
+    products_filter: "/storage/products/filter",
+    userDetails: "/user/details",
 };
