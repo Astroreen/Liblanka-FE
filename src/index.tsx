cabff4021f3389942187350e858df2a184462bc9
astroreen <astroreen@gmail.com>
2026-05-17 21:21:22 +0300

    fixed issues with new javascript version

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import i18next from "i18next";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { I18nextProvider } from "react-i18next";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./utils/reportWebVitals";
@@ -11,17 +9,11 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
-i18next.init({
-  interpolation: { escapeValue: false }, // React already does escape
-});
-
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      {/*Translator layer*/}
-      <I18nextProvider i18n={i18next}>
-        {/*Header on all pages*/}
-        <Helmet>
+      {/*Header on all pages*/}
+      <Helmet>
           <meta charSet="UTF-8" />
           <meta
             name="viewport"
@@ -38,7 +30,6 @@ root.render(
         </Helmet>
 
         <App />
-      </I18nextProvider>
     </HelmetProvider>
   </React.StrictMode>,
 );
